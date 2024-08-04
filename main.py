c972ee97b091233a973ffdd96e869fbf2060f8d0
BuildTools <chenxuzhang45@gmail.com>
2024-08-03 17:28:58 -0700

    revert upload

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from AutoGGUF import AutoGGUF
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = AutoGGUF()
-    window.show()
-    sys.exit(app.exec())
