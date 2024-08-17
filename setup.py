7966150d90defcbedbe5fc7f207dd51f2a5c05bd
BuildTools <chenxuzhang45@gmail.com>
2024-08-17 13:41:21 -0700

    ci: add PyPI setup script

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='AutoGGUF',
+    version='v1.7.1',
+    packages=[''],
+    url='https://github.com/leafspark/AutoGGUF',
+    license='apache-2.0',
+    author='leafspark',
+    author_email='',
+    description='automatically quant GGUF models',
+    install_requires=required,
+    entry_points={
+        'console_scripts': [
+            'autogguf-gui = src.main'
+        ]
+    }
+)
